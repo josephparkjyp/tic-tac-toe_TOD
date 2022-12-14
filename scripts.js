48b90c16709d13d6dcba7cd9c786d22fbc61fa1c
Joseph Park <66464574+josephparkjyp@users.noreply.github.com>
2022-12-14 15:36:56 -0800

    Edits to HTML and CSS. Complete JS logic for playing game

--- scripts.js
+++ scripts.js
@@ -16,10 +16,9 @@ function Game(player1, player2, player1Symbol) {
     }
     
     // Create the grid.
-    this.grid = [[null, null, null],
-                 [null, null, null], 
-                 [null, null, null]
-                ]
+    this.grid = [[1, 2, 3],
+                 [4, 5, 6], 
+                 [7, 8, 9]]
 
     // Create players.
     this.player1 = new Player(player1, player1Symbol)
@@ -29,25 +28,68 @@ function Game(player1, player2, player1Symbol) {
     this.currentPlayer = this.player1
     this.turn = 1
 
-    function currentPlayerSymbol() {
-        if (this.turn % 2 == 1) {
-            return this.player1.symbol    
-        } else if (this.turn % 2 == 0) {
-            return this.player2.symbol
-        }
-    }
-    function playRound(row, column) {
-        if (this.grid[row][column] !== Null) {
-            return 'Invalid Move. Try again.'
+    // Logic for how the round will be played.
+    // Basically: if current player is 1/2, play move then switch current player.
+    this.playRound = function(row, column) {
+        if (this.currentPlayer == this.player1 && this.grid[row][column] != 'X' && this.grid[row][column] != 'O') {
+            this.grid[row][column] = this.player1.symbol
+            this.potentialWinner = this.player1
+            this.currentPlayer = this.player2
+            this.turn++
+        } else if (this.currentPlayer == this.player2 && this.grid[row][column] != 'X' && this.grid[row][column] != 'O') {
+            this.grid[row][column] = this.player2.symbol
+            this.potentialWinner = this.player2
+            this.currentPlayer = this.player1
+            this.turn++
         } else {
-            this.grid[row][column] = currentPlayerSymbol()
-            this.turn += 1
+            return "Invalid move. Try again."
         }
+
+        // Check if the current board has a win pattern.
+        if (checkForWin(this.grid)) {
+            console.log(`${this.potentialWinner.name} won the game!`)
+        }
+        
+        console.table(this.grid)
     }
 
-    // Function to switch the status every move as well as increment turns total.
-    function playTurn() {
-        return Null
+    // Function that checks for a win.
+    function checkForWin(board) {
+        // Check rows
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (let i = 0; i < 3; i++) {
+            if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+            return true;
+        }
+        if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+            return true;
+        }
+
+        // No win
+        return false;
     }
+
+
+
+
+
+
 }
 
+
+
+
+
+const game = new Game('Joseph', 'Kimberly', 'O')
